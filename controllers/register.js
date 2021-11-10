0946580d5f2f749db8f2952d0ecfa9aea1b7afd7
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-10 08:34:55 +0200

    fixing the issue of cors

--- controllers/register.js
+++ controllers/register.js
@@ -2,30 +2,29 @@ export const handleRegister = (db, bcrypt) => (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(400).json('incorrect form submission');
-  } else {
-    const hash = bcrypt.hashSync(password);
-      db.transaction(trx => {
-        trx.insert({
-          hash: hash,
-          email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              name: name,
-              email: loginEmail[0],
-              joined: new Date()
-            })
-            .then(user => {
-              res.json(user[0]);
-            })
-              .then(trx.commit)
-              .catch(trx.rollback)
-            })
-            .catch(err => res.status(400).json('unable to register'));
+  }
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
       })
-    }
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+            .then(trx.commit)
+            .catch(trx.rollback)
+          })
+          .catch(err => res.status(400).json('unable to register'));
+    })
 }
