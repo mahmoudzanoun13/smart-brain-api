4b242fb6ae9e724a7a42a636c8616e7df96343d2
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-10 11:33:18 +0200

    fixing the issue of cors

--- server.js
+++ server.js
@@ -23,14 +23,8 @@ const db = knex({
 const app = express();
 
 app.use(cors({
-  origin: '*',
-  optionsSuccessStatus: 200
+  origin: '*'
 }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-});
 app.use(express.json());
 
 app.get('/', (req, res) => {res.json('it is working')})
