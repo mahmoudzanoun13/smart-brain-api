5e52c0467c57126194065b5dcff3a0286454cb70
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-10 11:18:23 +0200

    fixing the issue of cors

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ app.use(cors({
   origin: '*',
   optionsSuccessStatus: 200
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {res.json('it is working')})
