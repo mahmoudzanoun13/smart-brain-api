457d2c3aafd057d7e4b4afc20bbb71f023111c11
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-10 06:30:07 +0200

    solve cors issue

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {res.json('it is working')})
 app.post('/register', handleRegister(db, bcrypt))
