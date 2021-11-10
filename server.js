a71450c348ae5a67e241c66a6fb618b460d0554c
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-10 08:26:01 +0200

    fixing the issue of cors

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ const db = knex({
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://localhost:3000",
-  credentials: true
-}));
+app.use(cors());
 app.use(function (req, res, next) {
   res.set('Access-Control-Allow-Origin', 'https://localhost:3000');
   next();
