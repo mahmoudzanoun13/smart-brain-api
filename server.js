c53a35d28f6386e1888181fc488daf5245ade6bc
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-11 05:29:11 +0200

    fixing

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors({
 }));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'origin');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
