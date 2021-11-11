e1773f72596bc4c1ed07275ed745cba2aac16e7a
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-11 06:06:46 +0200

    fixing

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With, Content-Type'
     );
   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
   next();
