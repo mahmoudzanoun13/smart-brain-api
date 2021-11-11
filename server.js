974a60d784f32f1c199855dfc119f389cc48987c
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-11 03:53:03 +0200

    fixing

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
   next();
 });
 
