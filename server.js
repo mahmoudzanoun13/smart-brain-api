c7509fa87d2368dbbef14e723f1ecf2aeb302471
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-11 06:30:57 +0200

    fixing

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const db = knex({
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 /*app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
