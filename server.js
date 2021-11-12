18909de322f64cb8dd9c505871dce75f5aa8c056
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2021-11-12 08:54:33 +0200

    fixing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import {handleSignin} from './controllers/signin.js';
 import {handleProfileGet} from './controllers/profile.js';
 import {handleImage, handleApiCall} from './controllers/image.js';
 
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 const db = knex({
   client: 'pg',
   connection: {
